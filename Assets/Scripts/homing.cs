74510e4d614c52fc83ebddd35a67cc8cfca17f41
Xenose <xenose@live.com>
2020-01-18 23:21:49 +0900

    cleanned up more code...

--- Assets/Scripts/homing.cs
+++ Assets/Scripts/homing.cs
@@ -7,21 +7,27 @@ public class homing : MonoBehaviour {
 
 	// Speed in units per sec.
 	public float speed;
+	
+	GameObject _player;
 
-	void Update( ) {
+	private void Start() {
+		_player = GameObject.FindWithTag("Player");	
+	}
+
+	void FixedUpdate( ) {
 		// The step size is equal to speed times frame time.
 		float step = speed * Time.deltaTime;
 
 		// Move our position a step closer to the target.
-		transform.position = Vector3.MoveTowards( transform.position, GameObject.FindWithTag("Player").transform.position, step );
+		if (_player)
+			transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, step);
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		Debug.Log(collision.gameObject.tag);
 		if (collision.gameObject.tag == "Player") 
-			{
-				collision.gameObject.GetComponent<Entity>().Damage(100);
-			}
+		{
+			collision.gameObject.GetComponent<Entity>().Damage(100);
+		}
 	}
 }
